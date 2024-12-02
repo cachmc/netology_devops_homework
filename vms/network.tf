cde9776dd6e0ddb6293d54cb29391756249a5e2f
Vlad Shishkov <v.shishkov.19@gmail.com>
2024-12-02 01:03:50 +0000

    Removing unnecessary files

---
+++ vms/network.tf
@@ -0,0 +1,23 @@
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    shared_credentials_files = ["~/.aws/credentials"]
+    profile                  = "default"
+    region                   = "ru-central1"
+
+    bucket = "svm-tfstate"
+    key    = "ter-05/vps/terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+
+    endpoints = {
+      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gckqal9th70votrnub/etnqbr8fk7pv2uo65cos"
+      s3       = "https://storage.yandexcloud.net"
+    }
+
+    dynamodb_table = "tfstate_lock_develope"
+  }
+}
